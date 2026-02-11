6d525b67d7ba778be4ede7fe8100811356d8fcf2
Fredmaster08 <frxdmaster@gmail.com>
2026-02-11 11:47:47 +0100

    making particles

--- main.c
+++ main.c
@@ -15,9 +15,8 @@ int main() {
 
         ClearBackground(BLACK);
             
-        for(int i = 10; i < Particles; i++){
-           int gap = 10; 
-            DrawCircle(20 + gap, 20, 10, WHITE);
+        for(int i = 10; i < Particles; i + 10){
+            DrawCircle(20 + i, 20, 10, WHITE);
             
         }
         
