058e1c54409bc2d529a6a71e7b45351a8728aba3
Fredmaster08 <frxdmaster@gmail.com>
2026-02-11 11:42:33 +0100

    making particles

--- main.c
+++ main.c
@@ -1,9 +1,29 @@
 
+#include <raylib.h>
 #include <stdio.h>
 
 int main() {
 
-    printf("Hello Particle\n");
+    InitWindow(800, 600, "Particles");
+    SetTargetFPS(60);
+
+    int Particles = 100;
+
+    while(!WindowShouldClose()) {
+
+        BeginDrawing();
+
+        ClearBackground(BLACK);
+            
+        for(int i = 10; i < Particles; i++){
+           int gap = 10; 
+            DrawCircle(20 + gap, 20, 10, WHITE);
+            
+        }
+        
+        EndDrawing();
+    }
+    CloseWindow();
 
     return 0;
 }
