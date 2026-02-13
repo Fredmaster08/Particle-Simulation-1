d29cff3e1dd854cbee5a4acf7b5def4ee4a56a25
Fredmaster08 <frxdmaster@gmail.com>
2026-02-13 16:43:27 +0100

    working on vel & acc

--- main.c
+++ main.c
@@ -7,12 +7,28 @@ typedef struct Particle {
     Vector2 vel;
 }Particle;
 
-typedef struct {
+typedef struct verletObject {
     Vector2 current_position;
     Vector2 old_position;
     Vector2 acceleration
+}verletObject;
+
+typedef struct Solver {
+    
 }
 
+void updatePosition(float dt) {
+    const Vector2 velocity = current_position - old_position;
+    // saving current pos
+    old_position = current_position;
+    current_position = current_position + velocity + acceleration * dt * dt;
+    acceleration = {};
+}
+
+void accelerate(Vector2 acc) {
+    acceleration += acc;
+}
+    
 int main() {
 
     InitWindow(800, 600, "Particles");
