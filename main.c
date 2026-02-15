bb59ce2a2448d9ee34f840dfb94f81d11551bad1
Fredmaster08 <frxdmaster@gmail.com>
2026-02-15 14:13:24 +0100

    real particles

--- main.c
+++ main.c
@@ -1,23 +1,29 @@
 
 #include <raylib.h>
 #include <stdlib.h>
+#include <raymath.h>
 
 typedef struct Particle {
-    Vector2 pos;
-    Vector2 vel;
+    Vector2 pos_curr;
+    Vector2 pos_old;
+    Vector2 acc;
 }Particle;
 
 void update_particles(Particle* particles, int particles_count, float dt) {
     for(int i = 0; i < particles_count; i++) {
-        particles[i].pos.y += particles[i].vel.y * dt;
-        particles[i].vel.y += 9.81;
+        particles[i].acc = (Vector2){0, 1000.0f};
+        Vector2 velocity = Vector2Subtract(particles[i].pos_curr, particles[i].pos_old);
+        particles[i].pos_old = particles[i].pos_curr;
+        particles[i].pos_curr = Vector2Add(particles[i].pos_curr, Vector2Add(velocity, Vector2Scale(particles[i].acc, dt * dt)));
+        particles[i].acc = (Vector2){0};
     }
 
 }
 
 void draw_particles(Particle* particles, int particles_count) {
-    for(int i = 0; i < particles_count; i++) {
-        DrawCircle(particles[i].pos.x, particles[i].pos.y, 10.0f, WHITE);
+    for(int i = 0; i < particles_count - 1; i++) {
+ //       DrawCircle(particles[i].pos_curr.x, particles[i].pos_curr.y, 10.0f, WHITE);
+        DrawLineV(particles[i].pos_curr, particles[i + 1].pos_curr, WHITE);
     }
 }
 
@@ -29,6 +35,7 @@ void add_particle(Particle** particles, int* particles_count, Particle p) {
 
 int main() {
 
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(800, 600, "Particles");
     SetTargetFPS(60);
     int particles_count = 1;
@@ -36,10 +43,10 @@ int main() {
 
     while(!WindowShouldClose()) {
         
-        if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+        if(IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
             Particle p = {
-                .pos = GetMousePosition(),
-                .vel = {0, 0},
+                .pos_curr = GetMousePosition(),
+                .pos_old = p.pos_curr,
             };
             add_particle(&particles, &particles_count, p);
         }
